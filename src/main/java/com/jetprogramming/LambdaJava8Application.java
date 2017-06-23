1d969aa9b9b6af6469661f0fec35a062d0debe32
Jorge Tovar <jorgetovar621@gmail.com>
2017-06-23 15:30:20 -0500

    first commit

---
+++ src/main/java/com/jetprogramming/LambdaJava8Application.java
@@ -0,0 +1,12 @@
+package com.jetprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaJava8Application.class, args);
+	}
+}
